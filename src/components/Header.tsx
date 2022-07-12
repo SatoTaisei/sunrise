03e848afb983862fc51259f0b249257b9f754355
SatoTaisei <t.sei43rm@gmail.com>
2022-07-12 23:26:49 +0900

    💄

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { VFC } from "react";
 export const Header: VFC = () => {
   return (
     <header className="w-full h-16 bg-sky-500 flex items-center text-3xl pl-4">
-      <h1 className="text-3xl font-bold text-orange-500">Sunrise</h1>
+      <h1 className="text-3xl font-bold text-orange-300">Sunrise</h1>
     </header>
   );
 };
