c1db3e426db65175112b3edd75a33711f3254eed
SatoTaisei <t.sei43rm@gmail.com>
2022-07-14 08:40:41 +0900

    ♻️ MenuItemのコンポーネント化

---
+++ src/components/MenuItem.tsx
@@ -0,0 +1,21 @@
+import { VFC } from "react";
+
+import type { Menu } from "@/types/menu";
+
+export const MenuItem: VFC<{ menu: Menu }> = ({ menu }) => {
+  const { id, message, name, category } = menu;
+
+  return (
+    <div key={id} className="my-4">
+      <p className="text-sm text-orange-300 ml-9">{message}</p>
+      <div className="flex items-center">
+        <div className="w-7 h-7 bg-sky-500  rounded-full flex justify-center items-center mr-2">
+          <span className="text-sm text-white font-bold whitespace-nowrap scale-x-50">
+            {category}
+          </span>
+        </div>
+        <span className="font-bold text-2xl">{name}</span>
+      </div>
+    </div>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { client } from "@/libs/client";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
+import { MenuItem } from "@/components/MenuItem";
 
 import type { NextPage, GetStaticProps } from "next";
 import type { Menu } from "@/types/menu";
@@ -13,11 +14,7 @@ const Home: NextPage<{ menuList: Menu[] }> = ({ menuList }) => {
         <p className="text-center font-bold text-xl">- MENU -</p>
         <ul className="w-6/12 mx-auto">
           {menuList.map((menu) => (
-            <div key={menu.id} className="my-4">
-              <p className="text-sm text-neutral-500">{menu.message}</p>
-              <p className="font-bold text-2xl">{menu.name}</p>
-              <span>{menu.category}</span>
-            </div>
+            <MenuItem key={menu.id} menu={menu} />
           ))}
         </ul>
       </main>
