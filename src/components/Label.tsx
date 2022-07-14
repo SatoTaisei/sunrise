42aee48d7cb4c34e34892c4ae3a1cfda7705310c
SatoTaisei <t.sei43rm@gmail.com>
2022-07-14 17:54:30 +0900

    💄 ラベル修正

--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -1,10 +1,60 @@
 import { VFC } from "react";
 
 export const Label: VFC<{ category: string }> = ({ category }) => {
+  const setBackGroundColor = (category: string) => {
+    switch (category) {
+      case `日本酒`:
+        return `bg-blue-200`;
+      case `リキュール`:
+        return `bg-emerald-200`;
+      case `赤ワイン`:
+        return `bg-red-200`;
+      case `白ワイン`:
+        return `bg-yellow-300`;
+      case `スパークリングワイン`:
+        return `bg-cyan-200`;
+      case `ロゼワイン`:
+        return `bg-rose-200`;
+      case `焼酎`:
+        return `bg-amber-200`;
+      default:
+        break;
+    }
+  };
+
+  const setTextColor = (category: string) => {
+    switch (category) {
+      case `日本酒`:
+        return `text-blue-500`;
+      case `リキュール`:
+        return `text-emerald-500`;
+      case `赤ワイン`:
+        return `text-red-500`;
+      case `白ワイン`:
+        return `text-white`;
+      case `スパークリングワイン`:
+        return `text-cyan-500`;
+      case `ロゼワイン`:
+        return `text-rose-500`;
+      case `焼酎`:
+        return `text-amber-900`;
+      default:
+        break;
+    }
+  };
+
   return (
     <div className="w-16">
-      <div className="bg-blue-100 w-auto flex justify-center items-center mr-2 mt-2">
-        <span className="text-blue-500 text-xs font-bold whitespace-nowrap`">
+      <div
+        className={`${setBackGroundColor(
+          category
+        )} w-auto flex justify-center items-center mr-2 mt-2`}
+      >
+        <span
+          className={`${setTextColor(
+            category
+          )} text-xs font-bold whitespace-nowrap`}
+        >
           {category}
         </span>
       </div>
