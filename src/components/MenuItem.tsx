99ed13a495964a19c420c04136e295ec48e28dad
SatoTaisei <t.sei43rm@gmail.com>
2022-07-16 00:39:27 +0900

    ✨ Modalの追加

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,14 +1,14 @@
-import Link from "next/link";
 import { VFC } from "react";
 import { Label } from "@/components/Label";
+import { Modal } from "@/components/Modal";
 
 import type { Menu } from "@/types/menu";
 
 export const MenuItem: VFC<{ menu: Menu }> = ({ menu }) => {
-  const { id, message, name, category, img, description, tag } = menu;
+  const { message, name, category, img, description, tag } = menu;
 
   return (
-    <div key={id} className="my-6">
+    <div className="my-6">
       <div className="flex items-center">
         <Label category={category[0]} />
         {tag &&
@@ -18,15 +18,10 @@ export const MenuItem: VFC<{ menu: Menu }> = ({ menu }) => {
             </span>
           ))}
       </div>
-      {img && description ? (
-        <Link href={img.url}>
-          <a className="hover:text-sky-500">
-            <span className="block font-bold text-xl py-1">{name}</span>
-          </a>
-        </Link>
-      ) : (
-        <span className="block font-bold text-xl py-1">{name}</span>
-      )}
+      <div className="flex items-center">
+        <span className="inline-block font-bold text-xl py-1">{name}</span>
+        {img && description && <Modal imgURL={img.url} />}
+      </div>
       <p className="text-xs text-neutral-500">{message}</p>
     </div>
   );
