43e59267f1a4d6df4517e30282ce70aacc5de561
SatoTaisei <t.sei43rm@gmail.com>
2022-07-16 13:36:47 +0900

    ✨ ロゴの追加

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,28 @@
+import Image from "next/image";
 import { VFC } from "react";
+
+import Logo_ORANGE_PNG from "../../public/images/Logo_orange.png";
+import Logo_ORANGE_SVG from "../../public/images/Logo_orange.svg";
 
 export const Header: VFC = () => {
   return (
-    <header className="w-full h-16 bg-sky-500 flex items-center text-3xl pl-4">
-      <h1 className="text-3xl font-bold text-orange-300">Sunrise</h1>
+    <header className="w-full h-20 bg-blue-900 flex justify-center items-center text-3xl">
+      <h1 className="relative w-40 h-20 pt-4">
+        <picture>
+          <Image
+            src={Logo_ORANGE_SVG}
+            alt=""
+            layout="fill"
+            objectFit="contain"
+          />
+          <Image
+            src={Logo_ORANGE_PNG}
+            alt=""
+            layout="fill"
+            objectFit="contain"
+          />
+        </picture>
+      </h1>
     </header>
   );
 };
