a9ff9627fdbe0b2fa07621b7d4febf5f841da315
SatoTaisei <t.sei43rm@gmail.com>
2022-07-16 23:38:39 +0900

    🐛 headタグの追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,20 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>My page title</title>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, viewport-fit=cover"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
