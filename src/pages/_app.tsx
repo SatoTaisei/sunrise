9e938dce4ff3ca18c7b0e8051d06ca10b8756c09
SatoTaisei <t.sei43rm@gmail.com>
2022-07-17 09:15:08 +0900

    🚑

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>My page title</title>
+        <title>Menu | Sunrise</title>
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1.0, viewport-fit=cover"
