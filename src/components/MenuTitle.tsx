7387e4133cadd516b09572f82903d13c2bfbdee6
SatoTaisei <t.sei43rm@gmail.com>
2022-07-24 12:01:53 +0900

    💄

--- src/components/MenuTitle.tsx
+++ src/components/MenuTitle.tsx
@@ -2,9 +2,11 @@ import { VFC } from "react";
 
 export const MenuTitle: VFC<{ title: string }> = ({ title = "Wine" }) => {
   return (
-    <div className="relative">
-      <span className="font-extrabold text-9xl text-neutral-100">{title}</span>
-      <span className="absolute h-32 font-extrabold text-3xl leading-9 py-14 left-0">
+    <div className="relative mt-10">
+      <span className="inline-block font-extrabold text-9xl text-neutral-100">
+        {title}
+      </span>
+      <span className="absolute flex items-center h-32 font-extrabold text-3xl top-0">
         {title}
       </span>
     </div>
