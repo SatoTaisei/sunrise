d8b490e6e142abf68bac5f0459669afd9db52453
SatoTaisei <t.sei43rm@gmail.com>
2022-07-26 10:55:56 +0900

    💄

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import { VFC } from "react";
 
 export const Footer: VFC = () => {
   return (
-    <footer className="h-16 bg-blue-900 flex items-center justify-center text-m text-amber-500">
+    <footer className="h-16 bg-blue-900 flex items-center justify-center text-m font-bold text-yellow-500">
       <small>&copy; 2022 sunrise</small>
     </footer>
   );
