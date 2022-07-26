5336045287af2674d5dd93982865c14166270d1f
SatoTaisei <t.sei43rm@gmail.com>
2022-07-26 20:51:03 +0900

    ♻️

--- src/components/MenuByCategoryBlock.tsx
+++ src/components/MenuByCategoryBlock.tsx
@@ -7,11 +7,26 @@ import type { Menu } from "@/types/menu";
 export const MenuByCategoryBlock: VFC<{ byCategoryList: Menu[] }> = ({
   byCategoryList,
 }) => {
+  // kye:imgを持つメニューを上位に整列
+  const sortedWithImgMenuList: Menu[] = [];
+  // kye:imgを持つメニューを挿入
+  byCategoryList.map((menu) => {
+    if (Object.keys(menu).indexOf(`img`) !== -1) {
+      sortedWithImgMenuList.push(menu);
+    }
+  });
+  // kye:imgを持たないメニューを挿入
+  byCategoryList.map((menu) => {
+    if (Object.keys(menu).indexOf(`img`) === -1) {
+      sortedWithImgMenuList.push(menu);
+    }
+  });
+
   return (
     <div className="mt-16">
       <MenuTitle title={byCategoryList[0].category[0]} />
       <ul>
-        {byCategoryList.map((menu) => (
+        {sortedWithImgMenuList.map((menu) => (
           <MenuItem key={menu.id} menu={menu} />
         ))}
       </ul>
