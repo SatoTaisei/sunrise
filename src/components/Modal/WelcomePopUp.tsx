0baed19af9e12a2d05cab8a339a0507a95431f89
SatoTaisei <t.sei43rm@gmail.com>
2022-07-29 22:50:30 +0900

    💄

--- src/components/Modal/WelcomePopUp.tsx
+++ src/components/Modal/WelcomePopUp.tsx
@@ -25,11 +25,11 @@ export const WelcomePopUp: VFC<{
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
           >
-            <div className="fixed inset-0 bg-black bg-opacity-25" />
+            <div className="fixed inset-0" />
           </Transition.Child>
 
-          <div className="fixed inset-0 overflow-y-auto">
-            <div className="flex min-h-full items-center justify-center p-10 text-center">
+          <div className="fixed inset-0 overflow-y-auto cursor-pointer">
+            <div className="flex min-h-full items-center justify-center text-center">
               <Transition.Child
                 as={Fragment}
                 enter="ease-out duration-300"
@@ -39,7 +39,10 @@ export const WelcomePopUp: VFC<{
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95"
               >
-                <Dialog.Panel className="w-full h-80 flex items-center justify-center max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                <Dialog.Panel
+                  className="w-screen h-screen flex items-center justify-center transform overflow-hidden bg-black text-left align-middle shadow-xl transition-all"
+                  onClick={() => setIsPopUpOpen(false)}
+                >
                   {/* <Dialog.Title
                     as="h3"
                     className="text-3xl font-medium leading-6 text-gray-900"
