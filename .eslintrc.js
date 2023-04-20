7fbe5c9445d41662da16cc0d42d2670538d680e7
SatoTaisei <t.sei43rm@gmail.com>
2023-04-20 22:13:53 +0900

    🔨 add eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,28 +2,55 @@ module.exports = {
 	env: {
 		browser: true,
 		es2021: true,
-		node: true,
 	},
 	extends: [
 		'eslint:recommended',
-		'plugin:react/recommended',
 		'plugin:@typescript-eslint/recommended',
 		'next/core-web-vitals',
 		'prettier',
 	],
 	parser: '@typescript-eslint/parser',
 	parserOptions: {
-		ecmaFeatures: {
-			jsx: true,
-		},
 		ecmaVersion: 'latest',
 		sourceType: 'module',
 	},
-	plugins: ['react', '@typescript-eslint'],
-	rules: {},
-	settings: {
-		react: {
-			version: 'detect',
-		},
+	plugins: [
+		'strict-dependencies',
+		'@typescript-eslint',
+		'import',
+		'unused-imports',
+	],
+	rules: {
+		'unused-imports/no-unused-imports': 'warn',
+		'import/order': [
+			'warn',
+			{
+				groups: [
+					'builtin',
+					'external',
+					'internal',
+					'parent',
+					'sibling',
+					'index',
+					'object',
+					'type',
+				],
+				'newlines-between': 'always',
+				pathGroupsExcludedImportTypes: ['builtin'],
+				alphabetize: { order: 'asc', caseInsensitive: true },
+				pathGroups: [
+					{
+						pattern: 'src/types/**',
+						group: 'internal',
+						position: 'before',
+					},
+					{
+						pattern: 'src/repositories/**',
+						group: 'internal',
+						position: 'before',
+					},
+				],
+			},
+		],
 	},
 };
